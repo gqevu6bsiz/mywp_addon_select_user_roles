bbcda887ac8d4c8d17df8f4f0cc63eff3e230649
gqevu6bsiz <gqevu6bsiz@gmail.com>
2021-08-02 00:04:39 +0900

    Updated: PHP 8 Support.

--- mywp-select-user-roles/core/class.api.php
+++ mywp-select-user-roles/core/class.api.php
@@ -8,26 +8,6 @@ if ( ! class_exists( 'MywpSelectUserRolesApi' ) ) :
 
 final class MywpSelectUserRolesApi {
 
-  private static $instance;
-
-  private function __construct() {}
-
-  public static function get_instance() {
-
-    if ( !isset( self::$instance ) ) {
-
-      self::$instance = new self();
-
-    }
-
-    return self::$instance;
-
-  }
-
-  private function __clone() {}
-
-  private function __wakeup() {}
-
   public static function plugin_info() {
 
     $plugin_info = array(
--- mywp-select-user-roles/readme.txt
+++ mywp-select-user-roles/readme.txt
@@ -3,8 +3,8 @@ Contributors: gqevu6bsiz
 Donate link: http://gqevu6bsiz.chicappa.jp
 Tags: 
 Requires at least: 4.7
-Tested up to: 5.2
-Stable tag: 1.4
+Tested up to: 5.8
+Stable tag: 1.5.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
